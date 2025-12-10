abc65324e0ef82fdef776ff97501e8f7323dbe4a
HasnatGill <hasnatgill404@gmail.com>
2025-12-10 11:14:24 +0500

    Tweasks

--- router/shifts.js
+++ router/shifts.js
@@ -224,8 +224,8 @@ router.get("/live-operations", verifyToken, async (req, res) => {
         const user = await Users.findOne({ uid })
         if (!user) return res.status(401).json({ message: "Unauthorized access.", isError: true })
 
-        const currentTimeUTC = dayjs();
-        console.log('currentTimeUTC', currentTimeUTC)
+        const currentTimeUTC = dayjs.utc().startOf("day");
+        
         const startOfDayUTC = currentTimeUTC.startOf("day").toDate();
         const endOfDayUTC = currentTimeUTC.endOf("day").toDate();
 
